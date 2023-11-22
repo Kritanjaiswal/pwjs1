bd3755de6c21d3d25fb7880603420d9dd9994ea8
Kritanjaiswal <126539743+Kritanjaiswal@users.noreply.github.com>
2023-11-22 17:43:53 +0000

    learn about dataTypes

---
+++ basics1/datatypes.js
@@ -0,0 +1,18 @@
+
+//primitive data type (stack)
+const firstname="Kritan"
+const age=21
+const score=200
+let isLoggedin=false
+const nodeVersion=null
+let bankBalance=undefined
+const buttonid=symbol("id")
+const aReallyLongNumber=1246976543232n
+
+// reference (heap)
+const numbers=[1,2,3,4,5]
+const heroes=["superman","batman","flash",true,123]
+const userObjects={name:"Kritan",
+age:20,
+isLoggedIn:true
+}
