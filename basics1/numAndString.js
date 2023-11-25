c3eadb0d27155b0452958014657e7d0239006e59
Kritanjaiswal <126539743+Kritanjaiswal@users.noreply.github.com>
2023-11-25 06:19:40 +0000

    learnt about number and String

---
+++ basics1/numAndString.js
@@ -0,0 +1,15 @@
+const firstname='Kritan'
+const lastname="Jaiswal"
+console.log(firstname+' '+lastname);
+console.log(`My firstname is ${firstname} and last name is ${lastname}`);
+
+
+const username=new String('KritanJaiswal');
+console.log(username.length);
+console.log(username[0]);
+
+
+let email='kritanjaiswal9998@gmail.com';
+console.log(email);
+console.log(email.replace('kritan','rohit'));
+console.log(email.toUpperCase());
---
+++ basics1/typeConversion.js
@@ -0,0 +1,4 @@
+let rollNo='26'
+let rollNoinInt=parseInt(rollNo)
+console.log(typeof rollNo)
+console.log(typeof rollNoinInt)
