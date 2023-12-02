eebe905bf3528b29eaf4a41609bdfed85d70f4cf
Kritanjaiswal <126539743+Kritanjaiswal@users.noreply.github.com>
2023-12-02 10:19:19 +0000

    Learnt about Switch Case

---
+++ Week2/Switch.js
@@ -0,0 +1,22 @@
+let starFromUser="2";
+let getInInt=parseInt(starFromUser);
+switch(getInInt){
+    case 1:
+        console.log('very bad');
+        break;
+    case 2: 
+        console.log('bad');
+        break ;
+    case 3: 
+        console.log('Good');
+        break ;
+    case 4: 
+        console.log('Very Good');
+        break ;
+    case 5: 
+        console.log('Awesome');
+        break ;
+    default: 
+        console.log("I don't know the Rating");
+        break ;
+}
